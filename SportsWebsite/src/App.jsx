83f3a536dc7ae84406d49d2ea59def3c1c230482
IanLiu2603 <ian.liu2603@gmail.com>
2023-09-11 14:47:49 -0700

    We be routing

--- SportsWebsite/src/App.jsx
+++ SportsWebsite/src/App.jsx
@@ -1,11 +1,14 @@
 import React, { Component } from 'react'
+import { Routes, Route} from 'react-router-dom'
+import { NBA } from './Components/nba'
+import { nfl } from './Components/nfl'
 
 export class App extends Component {
   render() {
     return (
-      <div>
-        
-      </div>
+      <Routes>
+        <Route path='/' element={<NBA />}> </Route>
+      </Routes>
     )
   }
 }
