b7758dcdf24fc79a80b6d0a60dec99c8ddee803a
IanLiu2603 <ian.liu2603@gmail.com>
2023-09-11 14:54:16 -0700

    Working Links

--- SportsWebsite/src/App.jsx
+++ SportsWebsite/src/App.jsx
@@ -2,14 +2,18 @@ import React, { Component } from 'react'
 import { Routes, Route} from 'react-router-dom'
 import { NBA } from './Components/nba'
 import { NFL } from './Components/nfl'
+import { Topbar } from './Components/topbar'
 
 export class App extends Component {
   render() {
     return (
+      <div>
+      <Topbar />
       <Routes>
         <Route path='/' element={<NBA />}> </Route>
         <Route path='/NFL' element={<NFL />}> </Route>
       </Routes>
+      </div>
     )
   }
 }
--- SportsWebsite/src/Components/topbar.jsx
+++ SportsWebsite/src/Components/topbar.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export const Topbar = () => {
   return (
-    
+    <nav>
+        <Link to ='/'>NBA</Link>
+        <Link to ='/NFL'>NFL</Link>
+    </nav>
   )
 }
